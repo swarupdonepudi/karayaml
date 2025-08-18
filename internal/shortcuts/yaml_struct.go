fc0aeb214c00ce601c57eb33fead31bdb9b52046
Swarup Donepudi <swarup.donepudi@gmail.com>
2025-08-18 06:19:37 +0530

    changed the struct fields ordering to improve edit ux

--- internal/shortcuts/yaml_struct.go
+++ internal/shortcuts/yaml_struct.go
@@ -1,6 +1,6 @@
 package shortcuts
 
 type FileOpenShortcut struct {
-	Key  KeyBoardKey `yaml:"key" json:"key"`
 	File string      `yaml:"file" json:"file"`
+	Key  KeyBoardKey `yaml:"key" json:"key"`
 }
